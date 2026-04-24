f7d3897dee06a1369fcc7813a219a9446b44698b
Orquid3a <orquiburgos@gmail.com>
2026-04-24 11:18:42 -0400

    Update and rename Catalogo.java to Carreras.java

---
+++ Carreras.java
@@ -0,0 +1,13 @@
+
+public class Carrera {
+    private String nombre;
+    private String escuela;
+
+    public Carrera(String nombre, String escuela) {
+        this.nombre = nombre;
+        this.escuela = escuela;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getEscuela() { return escuela; }
+}
