fbf84c731618c2ba1dc1956e666f803b20c4ef60
GLevel <gdev0.rd@gmail.com>
2026-04-24 11:57:39 -0400

    Create Main.java
    Trae una lista de escuelas, y algunas instrucciones para mas tarde

---
+++ Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private List<Escuela> escuelas = new ArrayList<>();
+
+    public void run() 
+    {
+        //Array donde se creara cada escuela y segun lo guardado en la base de datos,
+        //para guardarlas en la lista escuelas.
+        for (int i = 0; i < 4; i++) {
+            escuelas.add(null);
+        }
+
+        //Crear el metodo para agregar cada carrera a nuestra escuela.
+    }
+}
